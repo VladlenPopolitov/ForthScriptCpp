42f0be4052c985505d78ef942bdcfb2ad7e4c737
Vladlen Popolitov <vladlenpopolitov@gmail.com>
2023-09-13 10:19:10 +0300

    Added XCHAR words

--- ForthScriptCpp.h
+++ ForthScriptCpp.h
@@ -760,7 +760,92 @@ CASE implementation https://forth-standard.org/standard/rationale#rat:core:SOURC
  *  Strings
 */
 // http://lars.nocrew.org/forth2012/xchar/X-SIZE.html 
+
+R"(HEX : wc, ( n low high -- ) 1+ , , , ;
+CREATE wc-table \ derived from wcwidth source code, for UCS32 
+0 0300 0357 wc,     0 035D 036F wc,     0 0483 0486 wc,
+0 0488 0489 wc,     0 0591 05A1 wc,     0 05A3 05B9 wc,
+0 05BB 05BD wc,     0 05BF 05BF wc,     0 05C1 05C2 wc,
+0 05C4 05C4 wc,     0 0600 0603 wc,     0 0610 0615 wc,
+0 064B 0658 wc,     0 0670 0670 wc,     0 06D6 06E4 wc,
+0 06E7 06E8 wc,     0 06EA 06ED wc,     0 070F 070F wc,
+0 0711 0711 wc,     0 0730 074A wc,     0 07A6 07B0 wc,
+0 0901 0902 wc,     0 093C 093C wc,     0 0941 0948 wc,
+0 094D 094D wc,     0 0951 0954 wc,     0 0962 0963 wc,
+0 0981 0981 wc,     0 09BC 09BC wc,     0 09C1 09C4 wc,
+0 09CD 09CD wc,     0 09E2 09E3 wc,     0 0A01 0A02 wc,
+0 0A3C 0A3C wc,     0 0A41 0A42 wc,     0 0A47 0A48 wc,
+0 0A4B 0A4D wc,     0 0A70 0A71 wc,     0 0A81 0A82 wc,
+0 0ABC 0ABC wc,     0 0AC1 0AC5 wc,     0 0AC7 0AC8 wc,
+0 0ACD 0ACD wc,     0 0AE2 0AE3 wc,     0 0B01 0B01 wc,
+0 0B3C 0B3C wc,     0 0B3F 0B3F wc,     0 0B41 0B43 wc,
+0 0B4D 0B4D wc,     0 0B56 0B56 wc,     0 0B82 0B82 wc,
+0 0BC0 0BC0 wc,     0 0BCD 0BCD wc,     0 0C3E 0C40 wc,
+0 0C46 0C48 wc,     0 0C4A 0C4D wc,     0 0C55 0C56 wc,
+0 0CBC 0CBC wc,     0 0CBF 0CBF wc,     0 0CC6 0CC6 wc,
+0 0CCC 0CCD wc,     0 0D41 0D43 wc,     0 0D4D 0D4D wc,
+0 0DCA 0DCA wc,     0 0DD2 0DD4 wc,     0 0DD6 0DD6 wc,
+0 0E31 0E31 wc,     0 0E34 0E3A wc,     0 0E47 0E4E wc,
+0 0EB1 0EB1 wc,     0 0EB4 0EB9 wc,     0 0EBB 0EBC wc,
+0 0EC8 0ECD wc,     0 0F18 0F19 wc,     0 0F35 0F35 wc,
+0 0F37 0F37 wc,     0 0F39 0F39 wc,     0 0F71 0F7E wc,
+0 0F80 0F84 wc,     0 0F86 0F87 wc,     0 0F90 0F97 wc,
+0 0F99 0FBC wc,     0 0FC6 0FC6 wc,     0 102D 1030 wc,
+0 1032 1032 wc,     0 1036 1037 wc,     0 1039 1039 wc,
+0 1058 1059 wc,     1 0000 1100 wc,     2 1100 115f wc,
+0 1160 11FF wc,     0 1712 1714 wc,     0 1732 1734 wc,
+0 1752 1753 wc,     0 1772 1773 wc,     0 17B4 17B5 wc,
+0 17B7 17BD wc,     0 17C6 17C6 wc,     0 17C9 17D3 wc,
+0 17DD 17DD wc,     0 180B 180D wc,     0 18A9 18A9 wc,
+0 1920 1922 wc,     0 1927 1928 wc,     0 1932 1932 wc,
+0 1939 193B wc,     0 200B 200F wc,     0 202A 202E wc,
+0 2060 2063 wc,     0 206A 206F wc,     0 20D0 20EA wc,
+2 2329 232A wc,     0 302A 302F wc,     2 2E80 303E wc,
+0 3099 309A wc,     2 3040 A4CF wc,     2 AC00 D7A3 wc,
+2 F900 FAFF wc,     0 FB1E FB1E wc,     0 FE00 FE0F wc,
+0 FE20 FE23 wc,     2 FE30 FE6F wc,     0 FEFF FEFF wc,
+2 FF00 FF60 wc,     2 FFE0 FFE6 wc,     0 FFF9 FFFB wc,
+0 1D167 1D169 wc,     0 1D173 1D182 wc,     0 1D185 1D18B wc,
+0 1D1AA 1D1AD wc,     2 20000 2FFFD wc,     2 30000 3FFFD wc,
+0 E0001 E0001 wc,     0 E0020 E007F wc,     0 E0100 E01EF wc,
+HERE wc-table - CONSTANT #wc-table
+DECIMAL
+\ inefficient table walk:
+: XC-WIDTH ( xchar -- n ) 
+   wc-table #wc-table OVER + SWAP ?DO 
+     DUP I 2@ WITHIN IF DROP I 2 CELLS + @ UNLOOP EXIT THEN 
+   3 CELLS +LOOP DROP 1 ; 
+)"
+
+": XC@+ ( xc-addr -- xc-addr' u ) "
+"COUNT DUP $80 U< IF EXIT THEN " // special case ASCII 
+"$7F AND $40 >R "
+" BEGIN DUP R@ AND WHILE R@ XOR "
+" 6 LSHIFT R> 5 LSHIFT >R >R COUNT "
+" $3F AND R> OR  REPEAT R> DROP ; "
+": XCHAR+ ( xc-addr -- xc-addr' ) XC@+ DROP ; "
+": +X/STRING ( xc-addr1 u1 -- xc-addr2 u2 ) OVER DUP XCHAR+ SWAP - /STRING ; "
+": X-WIDTH ( xc-addr u -- n ) 0 ROT ROT OVER + SWAP ?DO I XC@+ SWAP >R XC-WIDTH + R> I - +LOOP ; "
+": XC-SIZE ( xchar -- n ) DUP $80 U< IF DROP 1 EXIT THEN " // special case ASCII 
+"$800 2 >R BEGIN 2DUP U< NEGATE WHILE 5 LSHIFT R> 1+ >R DUP 0= UNTIL THEN 2DROP R> ; "
+": XC!+ ( xchar xc-addr -- xc-addr' ) "
+"OVER $80 U< IF TUCK C! CHAR+ EXIT THEN " // special case ASCII 
+">R 0 SWAP $3F BEGIN 2DUP U> WHILE "
+"2/ >R DUP $3F AND $80 OR SWAP 6 RSHIFT R> REPEAT $7F XOR 2* OR R> BEGIN OVER $80 U< 0= WHILE TUCK C! CHAR+ REPEAT NIP ; "
+": XC, ( xchar -- ) HERE XC!+ 1 cells allot  ! ; " // @bug? DP is not defined DP ! ; 
+
+": XC!+? ( xchar xc-addr u -- xc-addr' u' flag ) "
+">R OVER XC-SIZE R@ OVER U< IF ( xchar xc-addr1 len r: u1 ) " // not enough space 
+"DROP NIP R> FALSE ELSE >R XC!+ R> R> SWAP - TRUE THEN ; "
+": XEMIT ( xchar -- ) DUP $80 U< IF EMIT EXIT THEN " // special case ASCII 
+"0 SWAP $3F BEGIN 2DUP U> WHILE 2/ >R DUP $3F AND $80 OR "
+"SWAP 6 RSHIFT R> REPEAT $7F XOR 2* OR BEGIN DUP $80 U< 0= WHILE EMIT REPEAT DROP ; "
+"CREATE xholdbuf 8 ALLOT : XHOLD ( xchar -- ) xholdbuf TUCK XC!+ OVER - HOLDS ; "
+": XKEY ( -- xchar ) KEY DUP $80 U< IF EXIT THEN " // special case ASCII 
+"$7F AND $40 >R "
+"BEGIN DUP R@ AND WHILE R@ XOR 6 LSHIFT R> 5 LSHIFT >R >R KEY $3F AND R> OR REPEAT R> DROP ; "
 ": XCHAR- ( xc-addr -- xc-addr' ) BEGIN 1 CHARS - DUP C@ $C0 AND $80 <> UNTIL ; "
+": X\\STRING- ( xc-addr u -- xc-addr u' ) OVER + XCHAR- OVER - ; "
 ": X-SIZE ( xc-addr u1 -- u2 ) 0= IF DROP 0 EXIT THEN " // \ length of UTF-8 char starting at u8-addr (accesses only u8-addr) 
 " C@ "
 "  DUP $80 U< IF DROP 1 EXIT THEN "
@@ -5747,7 +5832,7 @@ if(0){
 							if (currentWord.size() > 0) {
 								if (!interpretNumbers(currentWord)){
 									// uncomment during debug if information is needed about wrong words
-									//std::cerr << "!!!Wrong word "<<currentWord<<std::endl;
+									std::cerr << "!!!Wrong word "<<currentWord<<std::endl;
 									push(CELL(-13));
 									exceptionsThrow();
 									xt = getDataCell(next_command);
@@ -5867,7 +5952,7 @@ if(0){
 			auto depth = dStack.stackDepth();
 #endif
 			REQUIRE_DSTACK_DEPTH(1, "RESTORE-INPUT");
-			Cell n = dStack.getTop(); pop();
+			Cell n = dStack.getTop(); dStack.pop();
 			REQUIRE_DSTACK_DEPTH(1, "RESTORE-INPUT");
 			Cell saveId= dStack.getTop(); ;
 			for (auto& save : savedInputVector) {
@@ -5950,8 +6035,8 @@ if(0){
 		void evaluate() {
 			REQUIRE_DSTACK_DEPTH(2, "EVALUATE");
 			SaveInput();
-			auto length = dStack.getTop(); pop();
-			auto caddr = CADDR(dStack.getTop()); pop();
+			auto length = dStack.getTop(); dStack.pop();
+			auto caddr = CADDR(dStack.getTop()); dStack.pop();
 			setSourceId(-1);
 			setSourceVariables(caddr, length, 0);
 			InterpretState = InterpretSource;
@@ -5960,8 +6045,8 @@ if(0){
 		void evaluateStart() {
 			REQUIRE_DSTACK_DEPTH(2, "EVALUATE");
 			SaveInput();
-			auto length = dStack.getTop(); pop();
-			auto caddr = CADDR(dStack.getTop()); pop();
+			auto length = dStack.getTop(); dStack.pop();
+			auto caddr = CADDR(dStack.getTop()); dStack.pop();
 			setSourceId(-1);
 			setDataCell(VarOffsetBlkAddress, Cell(0)); // set BLK=0
 			setSourceVariables(caddr, length, 0);
@@ -6439,7 +6524,7 @@ if(0){
 		}
 		void markerremove() {
 			REQUIRE_DSTACK_DEPTH(1, "MARKERREMOVE");
-			Cell defn = dStack.getTop(); pop();
+			Cell defn = dStack.getTop(); dStack.pop();
 			for (size_t i = defn; i<definitions.size(); ++i) {
 				auto& def = definitions[i];
 				def.name.append("_deleted");
